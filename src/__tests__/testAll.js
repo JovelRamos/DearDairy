ce4f9edddfefc77fed1a1e8b751c97a872810cd1
JovelRamos <98361343+JovelRamos@users.noreply.github.com>
2023-04-30 17:40:23 -0700

    Add files via upload

--- src/__tests__/testAll.js
+++ src/__tests__/testAll.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event';
-import App from './App';
 
 describe('Diary entry submission', () => {
   test('submitting a diary entry', async () => {
@@ -26,3 +27,4 @@ describe('Diary entry submission', () => {
     expect(textarea).toHaveValue('');
   });
 });
+

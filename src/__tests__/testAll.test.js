6aca1bcbc4cfac6ef33c40c35181d194e045e89f
JovelRamos <98361343+JovelRamos@users.noreply.github.com>
2023-05-07 10:33:55 -0700

    Add files via upload

--- src/__tests__/testAll.test.js
+++ src/__tests__/testAll.test.js
@@ -1,32 +1,64 @@
-import { render, fireEvent, waitFor, getByPlaceholderText, getByText } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import FoodEntryStack from '../components/FoodEntryStack';
+import FoodEntryForm from '../components/FoodEntryForm';
+import FoodEntry from '../components/FoodEntry';
+import App from '../App';
 
-test('renders the app without errors', () => {
-  render(<FoodEntryStack />);
-});
-
-test('adds a new entry to the diary', async () => {
-    const { getByText, findByText } = render(<FoodEntryStack />);
-    const foodDropdown = getByText('Select food'); // Change this line
-    
-    fireEvent.click(foodDropdown); // open dropdown menu
-    const searchInput = getByPlaceholderText('Search foods');
-    fireEvent.change(searchInput, { target: { value: 'Almonds' } }); // search for Almonds
-    fireEvent.click(getByText('Almonds')); // select Almonds
-    
-    const sizeInput = getByPlaceholderText('Size');
-    const servingUnitDropdown = getByText('Unit');
-    
-    fireEvent.change(sizeInput, { target: { value: '100' } });
-    fireEvent.click(servingUnitDropdown);
-    fireEvent.click(getByText('g'));
-    
-    fireEvent.click(foodDropdown); // open dropdown menu again
-    
-    const addButton = getByText('Add Food!');
-    fireEvent.click(addButton);
-    
-    await findByText('Almonds');
+describe('Food Entry Application', () => {
+    beforeEach(() => {
+      render(<App />);
+    });
+  
+    test('renders the application header', () => {
+      const headerElement = screen.getByText(/Dear Dairy/i);
+      expect(headerElement).toBeInTheDocument();
+    });
+  
+    test('renders the Food Entry form', () => {
+      const foodDropdown = screen.getByPlaceholderText(/Select food/i);
+      const sizeInput = screen.getByPlaceholderText(/Size/i);
+      const unitDropdown = screen.getByPlaceholderText(/Unit/i);
+      const addButton = screen.getByText(/Add Food!/i);
+      const submitButton = screen.getByText(/Submit Entry/i);
+  
+      expect(foodDropdown).toBeInTheDocument();
+      expect(sizeInput).toBeInTheDocument();
+      expect(unitDropdown).toBeInTheDocument();
+      expect(addButton).toBeInTheDocument();
+      expect(submitButton).toBeInTheDocument();
+    });
+  
+    test('adds a food entry to the table and displays the total row', async () => {
+      const foodDropdown = screen.getByPlaceholderText(/Select food/i);
+      const sizeInput = screen.getByPlaceholderText(/Size/i);
+      const unitDropdown = screen.getByPlaceholderText(/Unit/i);
+      const submitButton = screen.getByText(/Submit Entry/i);
+  
+      // Select the first food from the data
+      fireEvent.click(foodDropdown);
+      const foodOption = screen.getByText(new RegExp(data.data[0].food_name, 'i'));
+      fireEvent.click(foodOption);
+  
+      // Set the serving size
+      fireEvent.change(sizeInput, { target: { value: '1' } });
+  
+      // Select the first unit for the food
+      fireEvent.click(unitDropdown);
+      const unitOption = screen.getByText(/oz/i);
+      fireEvent.click(unitOption);
+  
+      // Submit the form
+      fireEvent.click(submitButton);
+  
+      // Check if the entry is added to the table
+      const foodEntry = screen.getByText(new RegExp(data.data[0].food_name, 'i'));
+      expect(foodEntry).toBeInTheDocument();
+  
+      // Check if the total row is displayed
+      const totalRow = screen.getByText(/Total/i);
+      expect(totalRow).toBeInTheDocument();
+    });
   });
-  
-  

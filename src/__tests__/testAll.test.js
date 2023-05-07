3b646360e882aca2b9c5639d0621b8449d07c973
JovelRamos <98361343+JovelRamos@users.noreply.github.com>
2023-05-07 10:14:34 -0700

    Add files via upload

--- src/__tests__/testAll.test.js
+++ src/__tests__/testAll.test.js
@@ -6,25 +6,27 @@ test('renders the app without errors', () => {
 });
 
 test('adds a new entry to the diary', async () => {
-  const { getByPlaceholderText, getByText, findByText } = render(<FoodEntryStack />);
-  const foodDropdown = getByPlaceholderText('Select food');
-
-  fireEvent.click(foodDropdown); // open dropdown menu
-  const searchInput = getByPlaceholderText('Search foods');
-  fireEvent.change(searchInput, { target: { value: 'Almonds' } }); // search for Almonds
-  fireEvent.click(getByText('Almonds')); // select Almonds
-
-  const sizeInput = getByPlaceholderText('Size');
-  const servingUnitDropdown = getByText('Unit');
-
-  fireEvent.change(sizeInput, { target: { value: '100' } });
-  fireEvent.click(servingUnitDropdown);
-  fireEvent.click(getByText('g'));
-
-  fireEvent.click(foodDropdown); // open dropdown menu again
-
-  const addButton = getByText('Add Food!');
-  fireEvent.click(addButton);
-
-  await findByText('Almonds');
-});
+    const { getByText, findByText } = render(<FoodEntryStack />);
+    const foodDropdown = getByText('Select food'); // Change this line
+    
+    fireEvent.click(foodDropdown); // open dropdown menu
+    const searchInput = getByPlaceholderText('Search foods');
+    fireEvent.change(searchInput, { target: { value: 'Almonds' } }); // search for Almonds
+    fireEvent.click(getByText('Almonds')); // select Almonds
+    
+    const sizeInput = getByPlaceholderText('Size');
+    const servingUnitDropdown = getByText('Unit');
+    
+    fireEvent.change(sizeInput, { target: { value: '100' } });
+    fireEvent.click(servingUnitDropdown);
+    fireEvent.click(getByText('g'));
+    
+    fireEvent.click(foodDropdown); // open dropdown menu again
+    
+    const addButton = getByText('Add Food!');
+    fireEvent.click(addButton);
+    
+    await findByText('Almonds');
+  });
+  
+  

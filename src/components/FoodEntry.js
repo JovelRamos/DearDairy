4cc7f9bbbb365701ec7bdfd0cc101b9d9e529fbc
JovelRamos <98361343+JovelRamos@users.noreply.github.com>
2023-05-07 12:37:09 -0700

    Add files via upload

--- src/components/FoodEntry.js
+++ src/components/FoodEntry.js
@@ -1,11 +1,10 @@
-// FoodEntry.js
 import React from 'react';
 import { Button, Icon, Table, Popup} from 'semantic-ui-react';
 
 function FoodEntry({ food, calories, proteins, fats, carbohydrates, deleteEntry, deleteButtonStyle }) {
   return (
     <Table.Row>
-      <Table.Cell textAlign="center">{food}</Table.Cell>
+  <Table.Cell data-testid="food-entry" textAlign="center">{food}</Table.Cell>
       <Table.Cell textAlign="center">{Math.round(calories)}</Table.Cell>
       <Table.Cell textAlign="center">{Math.round(proteins)}</Table.Cell>
       <Table.Cell textAlign="center">{Math.round(fats)}</Table.Cell>
